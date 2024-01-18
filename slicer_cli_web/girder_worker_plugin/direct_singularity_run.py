71648d9ef27ae1b9a6efd723d4747300a1e48ce1
suhasthegame <44828963+suhasthegame@users.noreply.github.com>
2024-01-18 15:14:22 -0500

    Added Pull_image function
    Added the pull_image_if_not_true function to handle cases where the image needs to be pulled if it's not present in the OS.

---
+++ slicer_cli_web/girder_worker_plugin/direct_singularity_run.py
@@ -0,0 +1,135 @@
+from os import R_OK, access, getenv, F_OK,chdir,system
+from os.path import abspath, basename, isfile, join
+
+from girder_worker.app import app
+from girder_worker.docker.io import FDReadStreamConnector
+from girder_worker.docker.tasks import DockerTask, _docker_run
+from girder_worker.docker.transforms import BindMountVolume, ContainerStdOut
+from girder_worker.docker.transforms.girder import GirderFileIdToVolume
+from girder_worker_utils import _walk_obj
+from girder_worker_utils.transforms.girder_io import GirderClientTransform
+from girder import logger
+from utils.singularity_helper import SINGULARITY_COMMANDS, singularity_cmd_list
+
+from .cli_progress import CLIProgressCLIWriter
+
+
+def _get_basename(filename, direct_path):
+    if filename:
+        return filename
+    if not direct_path:
+        return None
+    return basename(direct_path)
+
+
+TEMP_VOLUME_DIRECT_MOUNT_PREFIX = '/mnt/girder_direct_worker'
+
+
+class DirectGirderFileIdToVolume(GirderFileIdToVolume):
+    def __init__(self, _id, filename=None, direct_file_path=None, **kwargs):
+        superc = super()
+        superc.__init__(_id, filename=_get_basename(filename, direct_file_path),
+                        **kwargs)
+        self._direct_file_path = direct_file_path
+        self._direct_container_path = None
+
+    def resolve_direct_file_path(self):
+        if not self._direct_file_path:
+            return None
+
+        path = abspath(self._direct_file_path)
+        if isfile(path) and access(path, R_OK):
+            self._direct_container_path = join(TEMP_VOLUME_DIRECT_MOUNT_PREFIX, self._filename)
+            return BindMountVolume(path, self._direct_container_path, mode='ro')
+        return None
+
+    def transform(self, **kwargs):
+        if self._direct_container_path:
+            return self._direct_container_path
+        return super().transform(**kwargs)
+
+
+class GirderApiUrl(GirderClientTransform):
+    def transform(self, **kwargs):
+        return self.gc.urlBase
+
+
+class GirderToken(GirderClientTransform):
+    def transform(self, **kwargs):
+        return self.gc.token
+
+
+def _resolve_direct_file_paths(args, kwargs):
+    extra_volumes = []
+
+    def resolve(arg, **kwargs):
+        if isinstance(arg, DirectGirderFileIdToVolume):
+            path = arg.resolve_direct_file_path()
+            if path:
+                extra_volumes.append(path)
+        return arg
+    _walk_obj(args, resolve)
+    _walk_obj(kwargs, resolve)
+
+    return extra_volumes
+
+
+class DirectDockerTask(DockerTask):
+    def __call__(self, *args, **kwargs):
+        extra_volumes = _resolve_direct_file_paths(args, kwargs)
+        if extra_volumes:
+            volumes = kwargs.setdefault('volumes', [])
+            if isinstance(volumes, list):
+                # list mode use
+                volumes.extend(extra_volumes)
+            else:
+                for extra_volume in extra_volumes:
+                    volumes.update(extra_volume._repr_json_())
+
+        super().__call__(*args, **kwargs)
+
+class DirectSingularityTask(DockerTask):
+    def __call__(self,*args,**kwargs):
+        _resolve_direct_file_paths(args,kwargs)
+
+def _pull_image_if_not_present(image):
+    """
+    Pulls the specified Plugin image (Ususally a docker image) onto the singularity container if the image is not present
+    """
+    #Image path - Image path has to be present in tmp/images folder
+    if not image:
+        logger.write('Image name cannot be empty')
+        return False
+    imageLoc = getenv('TEMP_DIR', '/blue/pinaki.sarder/katarichalusuhas/singularity/tmp/images')
+    if not access(imageLoc,F_OK) or not access(imageLoc,R_OK):
+        logger.write(f"The image location is not accessible via girder. Please check the permissions of the filesystem")
+    #Change the current working directory to the destination location to check for and install the docker image
+    try:
+        chdir(imageLoc)
+        if not isfile(image + '.sif'):
+            pull_cmd_fn = singularity_cmd_list(SINGULARITY_COMMANDS['PULL'])
+            pull_cmd = pull_cmd_fn(image)
+            system(pull_cmd)
+    except Exception as e:
+        logger.write(f'Error occured {e}')
+    return True
+
+
+
+
+@app.task(base=DirectSingularityTask, bind=True)
+def run(task, **kwargs):
+    """Wraps singularity_run to support running singularity containers"""
+
+    pull_image = kwargs.get('pull_image')
+    if pull_image == 'if-not-present':
+        kwargs['pull_image'] = not _pull_image_if_not_present(kwargs['image'])
+
+    if hasattr(task, 'job_manager'):
+        stream_connectors = kwargs.setdefault('stream_connectors', [])
+        stream_connectors.append(FDReadStreamConnector(
+            input=ContainerStdOut(),
+            output=CLIProgressCLIWriter(task.job_manager)
+        ))
+
+    return _docker_run(task, **kwargs)
---
+++ utils/singularity_helper.py
@@ -0,0 +1,13 @@
+'''
+This file deals with all the helper functions to add help with adding singularity functionality to slicer_cli_web  
+'''
+
+SINGULARITY_COMMANDS  = {
+    'PULL': 0
+}
+
+def singularity_cmd_list(cmd):
+    def singularity_pull(imageName):
+        return f"singularity pull {imageName}.sif docker://{imageName}"
+    if cmd == SINGULARITY_COMMANDS['PULL']:
+        return singularity_pull
