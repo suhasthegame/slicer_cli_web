c98e72d82c55a9090a90c6e7092a431c50bf2737
suhasthegame <44828963+suhasthegame@users.noreply.github.com>
2024-04-08 16:01:12 -0400

    Added switch_to_sif_image_folder function

--- slicer_cli_web/singularity/utils.py
+++ slicer_cli_web/singularity/utils.py
@@ -1,3 +1,4 @@
+import os
 def is_valid_image_name_format(image_str:str):
     '''
     This function is used to validate whether the user supplied a valid string <img>:<tag> as an argument for functions like singularity pull
@@ -27,3 +28,25 @@ def generate_image_name_for_singularity(image_str:str):
         raise Exception('Not a valid image name. Please pass the image name in the format')
     image_str = image_str.replace('/','_').replace(':','_')
     return f"{image_str}.sif"
+
+def switch_to_sif_image_folder(image_path:str = ''):
+    '''
+    This function is used to handle Issues that is occuring when Singularity switches directory when running a plugin and not having the context of where the SIF IMAGES are located for subsequent image pulls. 
+    This function ensures that Singularity always looks for the plugins in the proper location
+    
+    Args: 
+    image_path (str, optional) - This parameter is highly optional and is not recommended unless a specific use-case arises in the future
+    
+    Returns:
+    None
+    
+    Raises: 
+    This function raises an Exception if the SIF_IMAGE_PATH env variable is not set. 
+    
+    '''
+    try:
+        if not image_path:
+            image_path = os.getenv('SIF_IMAGE_PATH')
+        os.chdir(image_path)
+    except Exception as e:
+        raise Exception(f"Please set the SIF_IMAGE_PATH environment variable to locate SIF images")
